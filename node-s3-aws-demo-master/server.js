6473f1ad955a847f9eda83f00a84e2cb349f8389
Ashwath Prabhu <prabhuashwath987@gmail.com>
2021-01-29 17:09:41 +0530

    initial commit

---
+++ node-s3-aws-demo-master/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const expressLayouts = require('express-ejs-layouts')
+const path = require('path')
+const app = express()
+const PORT = 5000
+
+app.use(expressLayouts)
+
+app.set('views', path.join(__dirname,'./src/views/layouts'))
+app.use('/public', express.static(__dirname + '/public'));
+app.set('view engine','ejs')
+app.use('/api/v1/aws-s3-upload-document', require('./src/router/controller'))
+
+// app.use('/', require('./src/router/controller'))
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
